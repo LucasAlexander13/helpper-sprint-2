9574dc87808890d1feca84706750c31092968af0
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-06 17:57:26 -0300

    Construção da classe em paradigma funcional

---
+++ Exercicio-07/funcional.ts
@@ -0,0 +1,27 @@
+class ListarValores {
+    lista: number[];
+
+    constructor(lista_inserida: any[]){
+        this.lista = lista_inserida.filter(valor => !isNaN(valor)).map(Number);
+    }
+
+    private menorValor() {
+        return Math.min(...this.lista);
+    }
+
+    private maiorValor() {
+        return Math.max(...this.lista);
+    }
+
+    private somatorio(valor: number, auxiliar: number){
+        return valor + auxiliar;
+    }
+
+    private valorMedio() {
+        return this.lista.reduce(this.somatorio, 0) / this.lista.length;
+    }
+
+    retornarValores(){
+        return [this.maiorValor, this.menorValor, this.valorMedio]
+    }
+}

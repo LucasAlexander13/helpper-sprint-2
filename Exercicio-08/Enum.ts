f27406f7e8a707fd85439604b9a2b869a3381e8e
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-11 17:40:54 -0300

    Estrutura EnumPessoa criada para os objetos

---
+++ Exercicio-08/Enum.ts
@@ -0,0 +1,12 @@
+/**
+ * EnumPessoa
+ * @enum string
+ * listagem de cientistas para a factory
+ */
+export enum EnumPessoa {
+    Lovelace = "Ada Lovelace",
+    Turing = "Alan Turing",
+    Tesla = "Nikola Tesla",
+    Copernico = "Nicolau Copernico",
+    Ichbiah = "Jean Ichbiah"
+}
---
+++ Exercicio-08/scripts/Enum.js
@@ -0,0 +1,14 @@
+/**
+ * EnumPessoa
+ * @enum string
+ * listagem de cientistas para a factory
+ */
+export var EnumPessoa;
+(function (EnumPessoa) {
+    EnumPessoa["Lovelace"] = "Ada Lovelace";
+    EnumPessoa["Turing"] = "Alan Turing";
+    EnumPessoa["Tesla"] = "Nikola Tesla";
+    EnumPessoa["Copernico"] = "Nicolau Copernico";
+    EnumPessoa["Ichbiah"] = "Jean Ichbiah";
+})(EnumPessoa || (EnumPessoa = {}));
+//# sourceMappingURL=Enum.js.map

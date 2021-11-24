8faf7f54e1a56af50cefb6bc53e12c547b60bde4
Lucas Alexander <lucasalexander13@hotmail.com>
2021-11-24 19:20:55 -0300

    Atribuida tipagem Pessoa | undefined para ada

--- Exercicio-08/script.ts
+++ Exercicio-08/script.ts
@@ -5,7 +5,7 @@ import { Pessoa } from "./Pessoa";
 //a) Após a leitura, crie uma Factory que, a partir da lista do exercício 6, 
 //dado um array, devolva o nome e a bio da pessoa.
 let fabrica: FabricaPessoa = new FabricaPessoa();
-let ada: Pessoa = fabrica.retornarPessoa(EnumPessoa.LOVELACE);
+let ada: Pessoa | undefined = fabrica.retornarPessoa(EnumPessoa.LOVELACE);
 console.log(ada.retornarInfo());
 
 //b) Crie um código que responda quem criou a linguagem de programação Ada, 
